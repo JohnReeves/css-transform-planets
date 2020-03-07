732790ab41fe44bc49320c8176d5b5c55b42bde3
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-03-07 22:14:27 +0000

    broken to show moon's movement

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // not needed to move planets with css transforms
+var earth = $("#earth");
+var moon = $("#moon");
+
+var cx = earth.attr("cx");
+var cy = earth.attr("cy");
+
+// use interval timers to update position of the moon
+
