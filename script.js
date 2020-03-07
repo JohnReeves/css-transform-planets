bb4bd029380775e1c5e1cf0fab2a05e912925e67
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-03-07 22:32:46 +0000

    calculate new planet position to them used the rotate transform

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ var moon = $("#moon");
 
 var cx = earth.attr("cx");
 var cy = earth.attr("cy");
-
+var count;
+console.log("hello");
 // use interval timers to update position of the moon
+function moveMoons(){
+count += 1;
+console.log(cx+", "+cy);
+moon.attr("cx", cx);
+moon.attr("cy", cy);
 
+
+};
+
+setInterval(moveMoons, 500);
