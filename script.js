bb6458c672747696b78a571aae7f5e6fa194d8b1
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-03-08 08:20:18 +0000

    does transform change attributes? nope

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 var earth = $("#earth");
 var moon = $("#moon");
 
-var cx = earth.attr("cx");
-var cy = earth.attr("cy");
+var cx;
+var cy;
 var count;
 console.log("hello");
-// use interval timers to update position of the moon
+
+// test interval timers to update position of the moon
 function moveMoons(){
 count += 1;
+ cx = earth.attr("cx");
+ cy = earth.attr("cy");
 console.log(cx+", "+cy);
 moon.attr("cx", cx);
 moon.attr("cy", cy);
-
 
 };
 
